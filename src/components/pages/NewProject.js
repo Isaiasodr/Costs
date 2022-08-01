d9e457c23dc6fdc0c5077e1df80a5f0307aae27f
Isaías Oliveira dos Reis <isaiasodr@gmail.com>
2022-08-01 13:23:48 -0300

    criação das flash message e início da estilização da página de projetos

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,32 @@
+import { useNavigate } from "react-router-dom";
 import ProjectForm from "../project/ProjectForm";
 import styles from "./NewProject.module.css";
 function NewProject() {
+  let navigate = useNavigate();
+  function createPost(project) {
+    /* inicializar o projetc cost an servoces */
+    project.cost = 0;
+    project.services = [];
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        navigate("/projects", {
+          state: { message: "projeto criado com sucesso!" },
+        });
+      })
+      .catch((err) => console.log(err));
+  }
   return (
     <div className={styles.newproject_container}>
       <h1>Criar projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-      <p>Formulário</p>
-      <ProjectForm btnText="Criar Projeto" />
+      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
     </div>
   );
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,26 @@
+import { useLocation } from "react-router-dom";
+import Message from "../layouts/message";
+import styles from "./Projects.module.css";
+import Container from "../layouts/Container";
+import Linkbutton from "../layouts/LinkButton";
 function Projects() {
-  return <h1>Projects</h1>;
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <Linkbutton to="/newproject" text="Criar Projeto" />
+      </div>
+      {message && <Message msg={message} type="success" />}
+      <Container customClass="start">
+        <p>projetos...</p>
+      </Container>
+    </div>
+  );
 }
 
 export default Projects;
